8f3f28b5e66f2062d0d3dd64d1b35c777f7e181a
shumbabrown <shumbabrownjc@gmail.com>
2018-11-07 23:34:13 -0800

    logic for displaying navbar

--- qoqa/src/app/app-main-nav/app-main-nav.component.ts
+++ qoqa/src/app/app-main-nav/app-main-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -8,9 +9,19 @@ import { map } from 'rxjs/operators';
   templateUrl: './app-main-nav.component.html',
   styleUrls: ['./app-main-nav.component.css'],
 })
-export class AppMainNavComponent {
+export class AppMainNavComponent implements OnInit {
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  public user = false;
+
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) {}
+
+  ngOnInit() {
+    console.log("calling ngOnInit in login app-main-nav.ts")
+
+    this.user = this.authService.getTestValue()
+    // this.user = this.authService.getUserDetails()
+    console.log(this.user)
+  }
 
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
