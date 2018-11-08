8d68c64b62582c0b51b345c8b0fbdc6facdbd2bd
shumbabrown <shumbabrownjc@gmail.com>
2018-11-07 23:40:07 -0800

    authorization logic

--- qoqa/src/app/app-main-nav/app-main-nav.component.ts
+++ qoqa/src/app/app-main-nav/app-main-nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../services/auth.service";
+import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -13,13 +14,24 @@ export class AppMainNavComponent implements OnInit {
 
   public user = false;
 
-  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) {}
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     console.log("calling ngOnInit in login app-main-nav.ts")
 
     this.user = this.authService.getTestValue()
     // this.user = this.authService.getUserDetails()
+
+
+    // To navigate to login if not logged in
+
+    // !!! Uncomment the if statement to turn on the authorization features
+    // !!! They are currently commented for debugging reasons.
+
+    // if (!this.user) {
+    //   this.router.navigate(['login'])
+    // }
+    
     console.log(this.user)
   }
 
